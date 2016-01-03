4d9fe8f3a9a1cbe513b6f34b3557def2ccf6eb46
Kobus van Schoor <pbscube@gmail.com>
2016-01-03 19:01:33 +0200

    First steps

--- src/bassoon.cpp
+++ src/bassoon.cpp
@@ -1,8 +1,11 @@
 #include "../include/bassoon.h"
+#include <dchain/dchain.h>
+#include <nihdb/nihdb.h>
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <cstdlib>
+#include <limits>
 
 void printHelp()
 {
@@ -19,10 +22,55 @@ int main(int argc, char* arcgs[])
 		std::cout << "Error, password database not found. Would you like to create one? [y/n]: ";
 		char ans;
 		std::cin >> ans;
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
 		if ( (ans != 'Y') &&  (ans != 'y')){
 			std::cerr << ".password file not created. Aborting...\n";
 			return 1;
+		}
+
+		config.close();
+		std::ofstream createDB(temp.c_str());
+		nihdb::dataBase tempDB(temp);
+		temp = "bassoon passsword database for ";
+		temp += std::getenv("USER");
+		tempDB.AddComment(temp);
+		tempDB.AddComment("All sensitive data is encrypted with dchain library");
+		tempDB.CreateSection("user");
+		tempDB.CreateVar("user", "name", std::getenv("USER"));
+		tempDB.CreateVar("user", "twostep", "false");
+		tempDB.CreateVar("user", "pass1");
+		tempDB.CreateVar("user", "pass2");
+		std::cout << "Do you want to use two-step authentication? (two passwords instead of one)? [y/n]: ";
+		std::cin >> ans;
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		if ( (ans == 'y') || (ans == 'Y')) {
+			tempDB.ChangeVarValue("user", "twostep", "true");
+		}
+
+		bool btemp = false;
+		std::string pass1, pass2;
+
+		std::cout << "Password will not be visible while you type them\n";
+		while (!btemp)
+		{
+			std::cout << "Enter password: ";
+			system("/usr/bin/stty -echo");
+			std::getline(std::cin, pass1);
+			std::cout << '\n';
+			system("/usr/bin/stty echo");
+			std::cout << "Confirm password: ";
+			system("/usr/bin/stty -echo");
+			std::getline(std::cin, pass2);
+			system("/usr/bin/stty echo");
+			std::cout << '\n';
+
+			btemp = (pass1 == pass2);
+			if (!btemp) {
+				std::cout << "Passwords do not match, please try again\n";
+			}
 		}
 	}
 }
